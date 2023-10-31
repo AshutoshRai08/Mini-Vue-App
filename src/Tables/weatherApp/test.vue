97fe4bd2048bfd13337272d664895c0ccff7dc96
Ashutosh Rai <rai.ashu0tosh11@gmail.com>
2023-10-31 16:12:54 +0530

    weather app

---
+++ src/Tables/weatherApp/test.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const firstName=ref("")
+const lastName=ref("")
+</script>
+
+
+<template>
+<div class="control">
+    <VInput v-model="firstName" aria-placeholder="fistName"></VInput>
+    <VInput aria-placeholder="lastName" v-model="lastName"></VInput>
+    <div v-if="firstName!='' || lastName!=''">{firstName.value}{lastName.value} </div>
+</div>
+</template>
