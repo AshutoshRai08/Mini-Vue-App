b346cfc0cda1997bfc45a0fb771c03b8876fa9b2
Ashutosh <rai.ashu0tosh11@gmail.com>
2024-02-22 12:30:34 +0530

    flex

---
+++ src/Tables/Components/flexAvatar.vue
@@ -0,0 +1,18 @@
+<template>
+    <VFlex flex-wrap="wrap" align-items="flex-end" row-gap=".5rem" column-gap=".25rem">
+        <VAvatar initials="VUE" size="big" color="warning" dot />
+        </VFlex>
+</template>
+
+<script lang="ts">
+import {defineProps} from 'vue'
+
+const props=defineProps<{
+vflex:Object;
+
+
+}
+>()
+
+
+</script>
