1914beee99f45636f141827e3c59181aa39d6c34
Ashutosh <rai.ashu0tosh11@gmail.com>
2024-02-24 23:24:56 +0530

    snackVue

---
+++ src/Tables/Components/snack.vue
@@ -0,0 +1,18 @@
+<template>
+    <VSnack title="Business" white size="large" placeholder="Asdhh" icon="feather:life-buoy">
+        <i class="iconify" data-icon="feather:plus"></i>
+        </VSnack>
+</template>
+
+<script lang="ts">
+import {defineProps} from 'vue'
+
+const props=defineProps<{
+vSnack:Object;
+
+
+}
+>()
+
+
+</script>
